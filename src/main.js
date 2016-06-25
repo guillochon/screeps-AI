cd1eac57197c962783f725c13449993728f2c820
guillochon <guillochon@gmail.com>
2016-06-25 15:35:04 -0400

    Added harvester create logic.

--- src/main.js
+++ src/main.js
@@ -3,6 +3,20 @@ var roleUpgrader = require('role.upgrader');
 var roleBuilder = require('role.builder');
 
 module.exports.loop = function () {
+    for(var name in Memory.creeps) {
+        if(!Game.creeps[name]) {
+            delete Memory.creeps[name];
+            console.log('Clearing non-existing creep memory:', name);
+        }
+    }
+
+    var harvesters = _.filter(Game.creeps, (creep) => creep.memory.role == 'harvester');
+    console.log('Harvesters: ' + harvesters.length);
+
+    if(harvesters.length < 2) {
+        var newName = Game.spawns.Spawn1.createCreep([WORK,CARRY,MOVE], undefined, {role: 'harvester'});
+        console.log('Spawning new harvester: ' + newName);
+    }
 
     var tower = Game.getObjectById('TOWER_ID');
     if(tower) {
